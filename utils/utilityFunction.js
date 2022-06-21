7db7b345c7df74bee7776e28f85a432c2353498c
Dendense <denyfarras@gmail.com>
2022-06-21 12:58:30 +0700

    Adding Local Storage Theme Switch Toggle

---
+++ utils/utilityFunction.js
@@ -0,0 +1,25 @@
+const slugify = function (text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
+}
+
+const truncate = function (text, length) {
+  return `${text.slice(0, length)}${text.length > length ? "..." : ""}`
+}
+
+function titleCase(str) {
+  return str?.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+module.exports = { slugify, truncate, titleCase }
